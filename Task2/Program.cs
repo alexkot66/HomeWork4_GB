757b754e2492429cfe22f6d1a4f6648d9aa8ad13
Alexandr Koshkarov <a.koshkarov.96@gmail.com>
2024-03-10 23:01:56 +0500

    added a solution to the second Task

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n.
+
+
+int Ack(int n, int m) // m - строка, n - столбец
+{
+    if (n == 0)
+    {
+        return (m + 1);
+    }
+    if (m == 0)
+    {
+        return Ack(n - 1, 1);
+    }
+    return Ack(n - 1, Ack(n, m - 1));
+
+}
+
+Console.WriteLine("Введите натуральное значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное значение M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int result = Ack(n, m);
+Console.WriteLine(result);
+
